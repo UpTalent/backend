f1aaccbb93d6013ffcbe04f5a37a565cba46d6db
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-22 23:38:49 +0200

    Add method toTalentProfileDTO in mapper TalentMapper

--- src/main/java/com/uptalent/mapper/TalentMapper.java
+++ src/main/java/com/uptalent/mapper/TalentMapper.java
@@ -2,6 +2,7 @@ package com.uptalent.mapper;
 
 import com.uptalent.talent.model.entity.Talent;
 import com.uptalent.talent.model.response.TalentDTO;
+import com.uptalent.talent.model.response.TalentProfileDTO;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TalentMapper {
     List<TalentDTO> toTalentDTOs(List<Talent> talents);
+    TalentProfileDTO toTalentProfileDTO(Talent talent);
 }
