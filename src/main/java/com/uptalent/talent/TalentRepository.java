e3b9708d8b14a8d94aad0b60e407fea5432a76a5
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-22 23:37:02 +0200

    Add method findByEmail in repository TalentRepository

--- src/main/java/com/uptalent/talent/TalentRepository.java
+++ src/main/java/com/uptalent/talent/TalentRepository.java
@@ -4,7 +4,11 @@ import com.uptalent.talent.model.entity.Talent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TalentRepository extends JpaRepository<Talent, Long> {
     boolean existsByEmailIgnoreCase(String email);
+
+    Optional<Talent> findByEmail(String email);
 }
