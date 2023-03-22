4536af6ec0f7589674f036456e8c4c9243f133c4
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-22 23:40:43 +0200

    Create methods for logging in system and getting profile in controller TalentController

--- src/main/java/com/uptalent/talent/controller/TalentController.java
+++ src/main/java/com/uptalent/talent/controller/TalentController.java
@@ -1,13 +1,26 @@
 package com.uptalent.talent.controller;
 
 import com.uptalent.pagination.PageWithMetadata;
+import com.uptalent.payload.HttpResponse;
 import com.uptalent.talent.TalentService;
+import com.uptalent.talent.exception.TalentExistsException;
+import com.uptalent.talent.exception.TalentNoFoundException;
+import com.uptalent.talent.model.request.TalentLoginRequest;
 import com.uptalent.talent.model.request.TalentRegistrationRequest;
 import com.uptalent.talent.model.response.TalentDTO;
+import com.uptalent.talent.model.response.TalentProfileDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.uptalent.jwt.JwtConstant.JWT_TOKEN_HEADER_NAME;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,10 +35,54 @@ public class TalentController {
         return talentService.getAllTalents(page, size);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void registerTalent(@Valid @RequestBody TalentRegistrationRequest talent){
-        talentService.addTalent(talent);
+    @GetMapping("/{talent-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TalentProfileDTO getTalentProfile(@PathVariable("talent-id") Long talentId){
+        return talentService.getTalentProfileById(talentId);
     }
 
+    @PostMapping
+    public ResponseEntity<?> registerTalent(@Valid @RequestBody TalentRegistrationRequest talent){
+        var response = talentService.addTalent(talent);
+        var headers = setJwtToHeader(response.jwtToken());
+
+        return new ResponseEntity<>(response, headers, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody TalentLoginRequest loginRequest){
+        var response = talentService.login(loginRequest);
+        var headers = setJwtToHeader(response.jwtToken());
+
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
+    }
+
+    private HttpHeaders setJwtToHeader(String token) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(JWT_TOKEN_HEADER_NAME, token);
+
+        return headers;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TalentNoFoundException.class)
+    public HttpResponse handlerNotFoundTalentException(Exception e) {
+        return new HttpResponse(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(TalentExistsException.class)
+    public HttpResponse handlerExistsTalentException(Exception e) {
+        return new HttpResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleFieldsException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
