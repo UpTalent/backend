f5d18a4402744d954e4e495768addc45242a56f9
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-24 19:27:05 +0200

    Add cors mapping

--- src/main/java/com/uptalent/UpTalentBackendApplication.java
+++ src/main/java/com/uptalent/UpTalentBackendApplication.java
@@ -2,6 +2,9 @@ package com.uptalent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UpTalentBackendApplication {
@@ -10,4 +13,13 @@ public class UpTalentBackendApplication {
 		SpringApplication.run(UpTalentBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer configurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
