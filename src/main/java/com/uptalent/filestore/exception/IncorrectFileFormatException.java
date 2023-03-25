a8752eaf270ffbf666783b7e36d083b98d417399
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-03-25 20:34:47 +0200

    Create FileStore exceptions

---
+++ src/main/java/com/uptalent/filestore/exception/IncorrectFileFormatException.java
@@ -0,0 +1,8 @@
+package com.uptalent.filestore.exception;
+
+public class IncorrectFileFormatException extends FileStoreException {
+    public IncorrectFileFormatException(String message) {
+        super(message);
+    }
+
+}
