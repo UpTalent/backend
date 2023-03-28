3ecb6c2024844f0083f6b479bb5970c0482e8183
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-28 21:49:25 +0300

    Change expiration time constant to 1 hour

--- src/main/java/com/uptalent/jwt/JwtConstant.java
+++ src/main/java/com/uptalent/jwt/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.uptalent.jwt;
 
 public class JwtConstant {
-    public static final long EXPIRATION_TIME = 10 * 60 * 1000; // 10-minutes
+    public static final long EXPIRATION_TIME = 3600 * 1000; // 1-hour
     public static final String TOKEN_HEADER = "Bearer ";
     public static final String JWT_TOKEN_HEADER_NAME = "Jwt-Token";
     public static final String TOKEN_ISSUE = "UpTalent";
