39109ade1027414dfb726646c6c382b93a1fc971
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-29 18:18:22 +0300

    Remove method for setting jwt into header

--- src/main/java/com/uptalent/talent/controller/TalentController.java
+++ src/main/java/com/uptalent/talent/controller/TalentController.java
@@ -3,22 +3,20 @@ package com.uptalent.talent.controller;
 import com.uptalent.filestore.FileStoreOperation;
 import com.uptalent.pagination.PageWithMetadata;
 import com.uptalent.talent.TalentService;
-import com.uptalent.talent.model.request.TalentEditRequest;
-import com.uptalent.talent.model.request.TalentLoginRequest;
-import com.uptalent.talent.model.request.TalentRegistrationRequest;
-import com.uptalent.talent.model.response.TalentDTO;
-import com.uptalent.talent.model.response.TalentOwnProfileDTO;
-import com.uptalent.talent.model.response.TalentProfileDTO;
+import com.uptalent.talent.model.request.TalentEdit;
+import com.uptalent.talent.model.request.TalentLogin;
+import com.uptalent.talent.model.request.TalentRegistration;
+import com.uptalent.talent.model.response.TalentGeneralInfo;
+import com.uptalent.talent.model.response.TalentOwnProfile;
+import com.uptalent.talent.model.response.TalentProfile;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import static com.uptalent.jwt.JwtConstant.JWT_TOKEN_HEADER_NAME;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,37 +26,35 @@ public class TalentController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public PageWithMetadata<TalentDTO> getAllTalents(@RequestParam(defaultValue = "0") int page,
-                                                     @RequestParam(defaultValue = "9") int size){
+    public PageWithMetadata<TalentGeneralInfo> getAllTalents(@RequestParam(defaultValue = "0") int page,
+                                                             @RequestParam(defaultValue = "9") int size){
         return talentService.getAllTalents(page, size);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TalentProfileDTO getTalentProfile(@PathVariable Long id){
+    public TalentProfile getTalentProfile(@PathVariable Long id){
         return talentService.getTalentProfileById(id);
     }
 
     @PostMapping
-    public ResponseEntity<?> registerTalent(@Valid @RequestBody TalentRegistrationRequest talent){
+    public ResponseEntity<?> registerTalent(@Valid @RequestBody TalentRegistration talent){
         var response = talentService.addTalent(talent);
-        var headers = setJwtToHeader(response.jwtToken());
 
-        return new ResponseEntity<>(response, headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody TalentLoginRequest loginRequest){
+    public ResponseEntity<?> login(@Valid @RequestBody TalentLogin loginRequest){
         var response = talentService.login(loginRequest);
-        var headers = setJwtToHeader(response.jwtToken());
 
-        return new ResponseEntity<>(response, headers, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TalentOwnProfileDTO updateTalent(@PathVariable Long id,
-                                            @Valid @RequestBody TalentEditRequest updatedTalent){
+    public TalentOwnProfile updateTalent(@PathVariable Long id,
+                                         @Valid @RequestBody TalentEdit updatedTalent){
         return talentService.updateTalent(id, updatedTalent);
     }
     @DeleteMapping("/{id}")
@@ -79,12 +75,5 @@ public class TalentController {
         talentService.uploadImage(id, image, operation);
     }
 
-    private HttpHeaders setJwtToHeader(String token) {
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.add(JWT_TOKEN_HEADER_NAME, token);
-
-        return headers;
-    }
 
 }
