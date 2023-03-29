7beacd8d9507a68abdeb757254aebb69492873df
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-29 18:37:48 +0300

    Update mapper

--- src/main/java/com/uptalent/mapper/TalentMapper.java
+++ src/main/java/com/uptalent/mapper/TalentMapper.java
@@ -1,16 +1,16 @@
 package com.uptalent.mapper;
 
 import com.uptalent.talent.model.entity.Talent;
-import com.uptalent.talent.model.response.TalentDTO;
-import com.uptalent.talent.model.response.TalentOwnProfileDTO;
-import com.uptalent.talent.model.response.TalentProfileDTO;
+import com.uptalent.talent.model.response.TalentGeneralInfo;
+import com.uptalent.talent.model.response.TalentOwnProfile;
+import com.uptalent.talent.model.response.TalentProfile;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TalentMapper {
-    List<TalentDTO> toTalentDTOs(List<Talent> talents);
-    TalentProfileDTO toTalentProfileDTO(Talent talent);
-    TalentOwnProfileDTO toTalentOwnProfileDTO(Talent talent);
+    List<TalentGeneralInfo> toTalentDTOs(List<Talent> talents);
+    TalentProfile toTalentProfileDTO(Talent talent);
+    TalentOwnProfile toTalentOwnProfileDTO(Talent talent);
 }
