b2f1892e2246d3eecd939d4d5941928bf78b29a7
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-03-31 00:20:34 +0300

    Add annotaions for TalentRegistration

--- src/main/java/com/uptalent/talent/model/request/TalentRegistration.java
+++ src/main/java/com/uptalent/talent/model/request/TalentRegistration.java
@@ -15,8 +15,10 @@ import java.util.Set;
 @Setter
 public class TalentRegistration {
     @NotBlank(message = "Lastname should not be blank")
+    @Size(max = 15, message = "Lastname must be less than 15 characters")
     String lastname;
     @NotBlank(message = "Firstname should not be blank")
+    @Size(max = 15, message = "Firstname must be less than 15 characters")
     String firstname;
     @NotNull(message = "Skills should not be null")
     Set<@NotBlank(message = "Name of skill should not be blank")
@@ -24,9 +26,11 @@ public class TalentRegistration {
     @NotNull(message = "Email should not be null")
     @NotBlank(message = "Email should not be blank")
     @Email(message = "Email should be valid")
+    @Size(max = 100, message = "Email must be less than 100 characters")
     String email;
     @NotNull(message = "Password should not be null")
     @NotBlank(message = "Password should not be blank")
     @Size(min = 6, message = "Password must be at least 6 characters long")
+    @Size(max = 32, message = "Password must be less than 32 characters")
     String password;
 }
