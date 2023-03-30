4c8bb035920af27d3c54e7ea44653c6d662d8b90
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-03-31 00:25:51 +0300

    Add new exception class EmptySkillsException

---
+++ src/main/java/com/uptalent/talent/model/exception/EmptySkillsException.java
@@ -0,0 +1,7 @@
+package com.uptalent.talent.model.exception;
+
+public class EmptySkillsException extends RuntimeException {
+    public EmptySkillsException(String message) {
+        super(message);
+    }
+}
