d155af7652322f6f7d003ef0f4e069cd5a83f184
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-03-31 00:20:17 +0300

    Add annotaions for TalentLogin

--- src/main/java/com/uptalent/talent/model/request/TalentLogin.java
+++ src/main/java/com/uptalent/talent/model/request/TalentLogin.java
@@ -6,9 +6,11 @@ public record TalentLogin(
         @NotNull(message = "Email should not be null")
         @NotBlank(message = "Email should not be blank")
         @Email(message = "Email should be valid")
+        @Size(max = 100, message = "Email must be less than 100 characters")
         String email,
         @NotNull(message = "Password should not be null")
         @NotBlank(message = "Password should not be blank")
         @Size(min = 6, message = "Password must be at least 6 characters long")
+        @Size(max = 32, message = "Password must be less than 32 characters")
         String password
 ) {}
