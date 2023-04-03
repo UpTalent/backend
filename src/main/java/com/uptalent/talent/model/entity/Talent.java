ca55b61545436ee2ad7722a65922e0774f32f3fa
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-03 20:48:26 +0300

    Create relationship between Talent and Proof entity(one to many)

--- src/main/java/com/uptalent/talent/model/entity/Talent.java
+++ src/main/java/com/uptalent/talent/model/entity/Talent.java
@@ -1,10 +1,12 @@
 package com.uptalent.talent.model.entity;
 
+import com.uptalent.proof.model.entity.Proof;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 import static jakarta.persistence.FetchType.EAGER;
@@ -19,18 +21,32 @@ public class Talent {
     @Id
     @GeneratedValue
     private Long id;
+
     private String lastname;
+
     private String firstname;
+
     private String avatar;
+
     private String banner;
+
     @ElementCollection(fetch = EAGER)
     private Set<String> skills;
+
     private String location;
+
     private String email;
+
     private String password;
+
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
     private String aboutMe;
 
+    @OneToMany(mappedBy = "talent",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Proof> proofs;
 }
