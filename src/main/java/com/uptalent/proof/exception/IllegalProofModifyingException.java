6fb1ae15a7242c1f9728fa43be8808f5c3176bff
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-05 20:33:51 +0300

    Create exception IllegalProofModifyingException

---
+++ src/main/java/com/uptalent/proof/exception/IllegalProofModifyingException.java
@@ -0,0 +1,7 @@
+package com.uptalent.proof.exception;
+
+public class IllegalProofModifyingException extends RuntimeException {
+    public IllegalProofModifyingException(String message) {
+        super(message);
+    }
+}
