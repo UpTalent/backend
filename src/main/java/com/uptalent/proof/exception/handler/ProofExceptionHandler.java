bf9fa6108e34a2ab886018e33c358fb01e5d5eb2
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-05 20:37:25 +0300

    Add handler method for handling IllegalProofModifyingException

--- src/main/java/com/uptalent/proof/exception/handler/ProofExceptionHandler.java
+++ src/main/java/com/uptalent/proof/exception/handler/ProofExceptionHandler.java
@@ -1,15 +1,17 @@
 package com.uptalent.proof.exception.handler;
 
 import com.uptalent.payload.HttpResponse;
+import com.uptalent.proof.exception.IllegalProofModifyingException;
 import com.uptalent.proof.exception.ProofNotFoundException;
 import com.uptalent.proof.exception.UnrelatedProofException;
+import com.uptalent.util.exception.handler.ExceptionHandlerController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ProofExceptionHandler {
+public class ProofExceptionHandler extends ExceptionHandlerController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProofNotFoundException.class)
     public HttpResponse handlerProofNotFoundException(ProofNotFoundException e) {
@@ -19,6 +21,12 @@ public class ProofExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UnrelatedProofException.class)
     public HttpResponse handlerUnrelatedProofException(UnrelatedProofException e) {
+        return new HttpResponse(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(IllegalProofModifyingException.class)
+    public HttpResponse handlerIllegalProofModifyingException(IllegalProofModifyingException e) {
         return new HttpResponse(e.getMessage());
     }
 }
