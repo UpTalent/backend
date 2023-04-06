a3031dba0a4175d4820f31caa163a9917b7ce319
Vadym <vadym.pratsybuda@nure.ua>
2023-04-06 20:27:26 +0300

    Add getAllProofs and getProofs methods to service and controller

--- src/main/java/com/uptalent/proof/controller/ProofController.java
+++ src/main/java/com/uptalent/proof/controller/ProofController.java
@@ -1,8 +1,10 @@
 package com.uptalent.proof.controller;
 
 
+import com.uptalent.pagination.PageWithMetadata;
 import com.uptalent.proof.model.request.ProofModify;
 import com.uptalent.proof.model.response.ProofDetailInfo;
+import com.uptalent.proof.model.response.ProofGeneralInfo;
 import com.uptalent.proof.service.ProofService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,13 @@ import java.net.URI;
 public class ProofController {
     private final ProofService proofService;
 
+    @GetMapping("/proofs")
+    @ResponseStatus(HttpStatus.OK)
+    public PageWithMetadata<ProofGeneralInfo> getAllProofs(@RequestParam(defaultValue = "0") int page,
+                                                           @RequestParam(defaultValue = "9") int size,
+                                                           @RequestParam(defaultValue = "desc") String sort) {
+        return proofService.getProofs(page, size, sort);
+    }
     @GetMapping("/talents/{talentId}/proofs/{proofId}")
     @ResponseStatus(HttpStatus.OK)
     public ProofDetailInfo getProofDetailInfo(@PathVariable Long talentId,
