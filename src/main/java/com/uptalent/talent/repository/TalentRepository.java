cd307ea659ef79d10c73342a35a078fbd7d9fdc8
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-08 11:17:57 +0300

    Add method verifyTalent for checking email with talentId in repository TalentRepository

--- src/main/java/com/uptalent/talent/repository/TalentRepository.java
+++ src/main/java/com/uptalent/talent/repository/TalentRepository.java
@@ -3,8 +3,10 @@ package com.uptalent.talent.repository;
 import com.uptalent.talent.model.entity.Talent;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,4 +17,7 @@ public interface TalentRepository extends JpaRepository<Talent, Long> {
     Optional<Talent> findByEmail(String email);
 
     Page<Talent> findAllByOrderByIdDesc(Pageable pageable);
+    @Query("select case when count(t) > 0 then true else false end " +
+            "from Talent t where t.id = ?1 and t.email = ?2")
+    boolean verifyTalent(Long id, String email);
 }
