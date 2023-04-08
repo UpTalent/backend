24eef9b92bf251445959d1e735c7f2328b98887f
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-08 11:13:55 +0300

    Fix error messages for validating request ProofModify

--- src/main/java/com/uptalent/proof/model/request/ProofModify.java
+++ src/main/java/com/uptalent/proof/model/request/ProofModify.java
@@ -13,10 +13,10 @@ public class ProofModify {
     @Size(max = 255, message = "Title must be less than 255 characters")
     private String title;
     @NotBlank(message = "Summary should not be blank")
-    @Size(max = 255, message = "Title must be less than 255 characters")
+    @Size(max = 255, message = "Summary must be less than 255 characters")
     private String summary;
     @NotBlank(message = "Content should not be blank")
-    @Size(max = 5000, message = "Title must be less than 5000 characters")
+    @Size(max = 5000, message = "Content must be less than 5000 characters")
     private String content;
     @NotNull(message = "Icon number should not be null")
     @Positive(message = "Icon number should be positive")
