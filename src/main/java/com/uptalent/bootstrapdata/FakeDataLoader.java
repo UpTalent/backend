ca7aa875e58e3ab41d3387fd64d58598db8dc17c
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-19 14:45:04 +0300

    Update FakeDataLoader

--- src/main/java/com/uptalent/bootstrapdata/FakeDataLoader.java
+++ src/main/java/com/uptalent/bootstrapdata/FakeDataLoader.java
@@ -83,7 +83,6 @@ public class FakeDataLoader implements CommandLineRunner {
                 .status(ProofStatus.PUBLISHED)
                 .published(faker.date().past(5, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
                 .talent(talent)
-                .kudos(0)
                 .build();
         return proofRepository.save(proof);
     }
