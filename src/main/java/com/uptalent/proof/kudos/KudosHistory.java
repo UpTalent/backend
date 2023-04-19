21ed09f1f28506d9e921771f653a7f9bcd882ad8
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-19 14:14:53 +0300

    Create entity KudosHistory

---
+++ src/main/java/com/uptalent/proof/kudos/KudosHistory.java
@@ -0,0 +1,28 @@
+package com.uptalent.proof.kudos;
+
+import com.uptalent.proof.model.entity.Proof;
+import com.uptalent.talent.model.entity.Talent;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KudosHistory {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "talent_id", referencedColumnName = "id")
+    private Talent talent;
+    @ManyToOne
+    @JoinColumn(name = "proof_id", referencedColumnName = "id")
+    private Proof proof;
+    private LocalDateTime sent;
+    private Integer kudos;
+}
