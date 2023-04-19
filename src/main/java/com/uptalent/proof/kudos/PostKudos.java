b19d74ec98b6aeeff01c0d64a90d9b422b7d6409
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-19 14:14:16 +0300

    Create request model PostKudos

---
+++ src/main/java/com/uptalent/proof/kudos/PostKudos.java
@@ -0,0 +1,16 @@
+package com.uptalent.proof.kudos;
+
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostKudos {
+    @Positive(message = "Number of kudos should be positive")
+    private int kudos;
+}
