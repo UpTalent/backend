85db3655ab5b6b8f406fec8bff67e2b50f29b2b7
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-22 19:32:34 +0300

    Update KudosHistoryRepository

--- src/main/java/com/uptalent/proof/kudos/repository/KudosHistoryRepository.java
+++ src/main/java/com/uptalent/proof/kudos/repository/KudosHistoryRepository.java
@@ -2,18 +2,15 @@ package com.uptalent.proof.kudos.repository;
 
 import com.uptalent.proof.kudos.model.entity.KudosHistory;
 import com.uptalent.proof.kudos.model.response.KudosSender;
-import com.uptalent.talent.model.entity.Talent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface KudosHistoryRepository extends JpaRepository<KudosHistory, Long> {
     @Query("select case when count(kh) > 0 then true else false end " +
             "from kudos_history kh where kh.sponsor.id = ?1 and kh.proof.id = ?2")
-    Boolean pressedProofBySponsorId(Long talentId, Long proofId);
+    Boolean pressedProofBySponsorId(Long sponsorId, Long proofId);
 
     @Query("SELECT new com.uptalent.proof.kudos.model.response.KudosSender" +
             "(kh.sponsor.fullname, kh.sponsor.avatar, kh.sent, kh.kudos) " +
