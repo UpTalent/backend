56c84fa541f3a5053096b2fc00d198bd14022e7a
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-23 12:22:11 +0300

    Create exception SponsorNotFoundException

---
+++ src/main/java/com/uptalent/sponsor/exception/SponsorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.uptalent.sponsor.exception;
+
+public class SponsorNotFoundException extends RuntimeException {
+    public SponsorNotFoundException(String message) {
+        super(message);
+    }
+}
