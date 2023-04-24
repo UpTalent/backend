497ce157b8aa9bb3b6958993f2b473d3b05d0195
CherkasovAndrii <andreyc406@gmail.com>
2023-04-24 20:25:12 +0300

    create sponsor profile model

---
+++ src/main/java/com/uptalent/sponsor/model/response/SponsorProfile.java
@@ -0,0 +1,20 @@
+package com.uptalent.sponsor.model.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class SponsorProfile {
+    private Long id;
+    private String fullname;
+    private String avatar;
+    private String email;
+    private int kudos;
+
+}
