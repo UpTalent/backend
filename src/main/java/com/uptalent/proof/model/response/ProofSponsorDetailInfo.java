43c92c0e96daaca1b590ce081217f1752ada7a7a
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-01 19:57:50 +0300

    Update ProofSponsorDetailInfo

--- src/main/java/com/uptalent/proof/model/response/ProofSponsorDetailInfo.java
+++ src/main/java/com/uptalent/proof/model/response/ProofSponsorDetailInfo.java
@@ -1,19 +1,22 @@
-package com.uptalent.proof.model.response;
+    package com.uptalent.proof.model.response;
 
-import com.uptalent.proof.model.enums.ProofStatus;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+    import com.uptalent.proof.model.enums.ProofStatus;
+    import com.uptalent.skill.model.SkillInfo;
+    import lombok.Data;
+    import lombok.EqualsAndHashCode;
 
-import java.time.LocalDateTime;
+    import java.time.LocalDateTime;
+    import java.util.Set;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
-public class ProofSponsorDetailInfo extends ProofDetailInfo {
-    private long sumKudosFromMe;
+    @EqualsAndHashCode(callSuper = true)
+    @Data
+    public class ProofSponsorDetailInfo extends ProofDetailInfo {
+        private long sumKudosFromMe;
 
-    public ProofSponsorDetailInfo(Long id, Integer iconNumber, String title, String summary, String content,
-                                  LocalDateTime published, int kudos, ProofStatus status, long sumKudosFromMe) {
-        super(id, iconNumber, title, summary, content, published, kudos, status);
-        this.sumKudosFromMe = sumKudosFromMe;
+        public ProofSponsorDetailInfo(Long id, Integer iconNumber, String title, String summary, String content,
+                                      LocalDateTime published, int kudos, ProofStatus status, long sumKudosFromMe,
+                                      Set<SkillInfo> skills) {
+            super(id, iconNumber, title, summary, content, published, kudos, status, skills);
+            this.sumKudosFromMe = sumKudosFromMe;
+        }
     }
-}
