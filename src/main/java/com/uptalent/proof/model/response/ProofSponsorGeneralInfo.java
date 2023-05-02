8d8a037c39ebbd86f35e24002d11a7e9c9fa4aad
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-02 14:09:20 +0300

    Update ProofSponsorGeneralInfo

--- src/main/java/com/uptalent/proof/model/response/ProofSponsorGeneralInfo.java
+++ src/main/java/com/uptalent/proof/model/response/ProofSponsorGeneralInfo.java
@@ -1,10 +1,8 @@
 package com.uptalent.proof.model.response;
 
-import com.uptalent.skill.model.SkillInfo;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -12,9 +10,8 @@ public class ProofSponsorGeneralInfo extends ProofGeneralInfo {
     private long sumKudosFromMe;
 
     public ProofSponsorGeneralInfo(Long id, Integer iconNumber, String title,
-                                   String summary, int kudos, LocalDateTime published, long sumKudosFromMe,
-                                   Set<SkillInfo> skills) {
-        super(id, iconNumber, title, summary, kudos, published, skills);
+                                   String summary, int kudos, LocalDateTime published, long sumKudosFromMe) {
+        super(id, iconNumber, title, summary, kudos, published);
         this.sumKudosFromMe = sumKudosFromMe;
     }
 }
