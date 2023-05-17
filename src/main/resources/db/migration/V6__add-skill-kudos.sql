a414b2e8afabff7c73254c7052fe10c376b6548d
CherkasovAndrii <andreyc406@gmail.com>
2023-05-17 14:44:00 +0300

    change migration

--- src/main/resources/db/migration/V6__add-skill-kudos.sql
+++ src/main/resources/db/migration/V6__add-skill-kudos.sql
@@ -2,7 +2,7 @@ create table if not exists skill_kudos(
     id serial not null,
     skill_id bigint not null,
     proof_id bigint not null,
-    kudos bigint not null default 0,
+    kudos bigint not null,
     primary key (id),
     foreign key (skill_id) references skill(id) on delete cascade,
     foreign key (proof_id) references proof(id) on delete cascade
