6b317419649352e44070fa56cff8ceadea2e4b60
CherkasovAndrii <andreyc406@gmail.com>
2023-05-17 14:29:22 +0300

    create SkillKudosRepository

---
+++ src/main/java/com/uptalent/skill/repository/SkillKudosRepository.java
@@ -0,0 +1,8 @@
+package com.uptalent.skill.repository;
+
+
+import com.uptalent.skill.model.entity.SkillKudos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillKudosRepository extends JpaRepository<SkillKudos, Long> {
+}
