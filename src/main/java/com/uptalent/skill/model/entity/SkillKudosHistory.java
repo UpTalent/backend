aa7b1ed4d4a2efa7e477e7643706b15fd7e14e1c
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-18 13:34:00 +0300

    Create SkillKudosHistory

---
+++ src/main/java/com/uptalent/skill/model/entity/SkillKudosHistory.java
@@ -0,0 +1,27 @@
+package com.uptalent.skill.model.entity;
+
+import com.uptalent.proof.kudos.model.entity.KudosHistory;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "skill_kudos_history")
+@Table(name = "skill_kudos_history")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SkillKudosHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "skillKudosHistory")
+    private Skill skill;
+
+    @ManyToOne
+    @JoinColumn(name = "kudos_history_id", referencedColumnName = "id")
+    private KudosHistory kudosHistory;
+
+    private Long kudos;
+}
