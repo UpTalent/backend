64cbef4d2f3d50b3738880622070187b2db36557
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-18 13:39:23 +0300

    Update PostKudos

--- src/main/java/com/uptalent/proof/kudos/model/request/PostKudos.java
+++ src/main/java/com/uptalent/proof/kudos/model/request/PostKudos.java
@@ -1,18 +1,18 @@
 package com.uptalent.proof.kudos.model.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostKudos {
-    @Max(value = 1999999999, message = "Kudos value reached maximum")
-    @Positive(message = "Number of kudos should be positive")
-    private int kudos;
+    @Size(max=30, message = "List of skills should be less than 30 items")
+    private List<PostKudosSkill> postKudosSkills;
 }
