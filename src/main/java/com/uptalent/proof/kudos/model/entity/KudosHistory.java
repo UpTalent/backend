55a0bf50a276041e45c051535bf6d9b4dc9ea294
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-18 13:38:48 +0300

    Update KudosHistory

--- src/main/java/com/uptalent/proof/kudos/model/entity/KudosHistory.java
+++ src/main/java/com/uptalent/proof/kudos/model/entity/KudosHistory.java
@@ -1,12 +1,14 @@
 package com.uptalent.proof.kudos.model.entity;
 
 import com.uptalent.proof.model.entity.Proof;
+import com.uptalent.skill.model.entity.SkillKudosHistory;
 import com.uptalent.sponsor.model.entity.Sponsor;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity(name = "kudos_history")
 @Table(name = "kudos_history")
@@ -33,5 +35,8 @@ public class KudosHistory {
 
     @ColumnDefault("0")
     @Column(nullable = false, name = "kudos")
-    private int kudos;
+    private Long totalKudos;
+
+    @OneToMany(mappedBy = "kudosHistory")
+    private List<SkillKudosHistory> skillKudosHistories;
 }
