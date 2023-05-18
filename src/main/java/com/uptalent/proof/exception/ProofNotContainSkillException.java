28641f7e5de4ee2a9d7ec1d2262538efe109fb6e
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-18 13:40:10 +0300

    Create ProofNotContainSkillException

---
+++ src/main/java/com/uptalent/proof/exception/ProofNotContainSkillException.java
@@ -0,0 +1,7 @@
+package com.uptalent.proof.exception;
+
+public class ProofNotContainSkillException extends RuntimeException{
+    public ProofNotContainSkillException(String message) {
+        super(message);
+    }
+}
