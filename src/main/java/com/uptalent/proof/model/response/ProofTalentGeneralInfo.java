9e8a9cec1d133d1d609c93f25cc37868efb2e8ee
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-18 13:36:18 +0300

    Update ProofTalentGeneralInfo

--- src/main/java/com/uptalent/proof/model/response/ProofTalentGeneralInfo.java
+++ src/main/java/com/uptalent/proof/model/response/ProofTalentGeneralInfo.java
@@ -13,7 +13,7 @@ public class ProofTalentGeneralInfo extends ProofGeneralInfo {
     private boolean myProof;
 
     public ProofTalentGeneralInfo(Long id, Integer iconNumber, String title,
-                                  String summary, int kudos, LocalDateTime published, boolean myProof,
+                                  String summary, long kudos, LocalDateTime published, boolean myProof,
                                   Set<SkillProofInfo> skills) {
         super(id, iconNumber, title, summary, kudos, published, skills);
         this.myProof = myProof;
