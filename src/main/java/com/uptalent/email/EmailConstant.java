c0ad66252290d92b34ae85639d1f1689dd82e6fa
Vadym <vadym.pratsybuda@nure.ua>
2023-05-24 14:32:39 +0300

    Update emailSender to send verify emails

--- src/main/java/com/uptalent/email/EmailConstant.java
+++ src/main/java/com/uptalent/email/EmailConstant.java
@@ -10,7 +10,30 @@ public class EmailConstant {
     @Value("${spring.mail.username}")
     private String email;
     public static String ADMIN_MAIL;
-    public static final String SUBJECT = "Your UpTalent account was temporary deleted";
+    public static final String SUBJECT_RESTORE = "Your UpTalent account was temporary deleted";
+    public static final String SUBJECT_VERIFY = "Your UPTalent account not activated";
+    public static final String MESSAGE_BEGIN = """
+                    <div style="display: grid;\040
+                        flex-direction: column;\040
+                        border-radius: 10px;\040
+                        padding: 20px;\040
+                        font-size: larger;\040
+                        border: 3px solid #48bde2;\040
+                        margin: 3px;">
+                        <h1 style="color: #48bde2;\040
+                        margin: 0;">""";
+    public static final String MESSAGE_END = """
+                <h4 style="margin-bottom: 0;">
+                            Best regards,
+                        </h4>
+                        <div style="display: flex; align-items: center; gap: 5px;">
+                        <img src="https://drive.google.com/uc?export=view&id=1Fx1E7h2r8ly23p5LCD5Jpc_slgUPSu2I" alt="logo" style="width:30px; object-fit: contain;"/>
+                        <h4 style="margin-left: 5px;">
+                            UPTALENT
+                        </h4>
+                        </div>
+                        </div>
+                """;
 
     //public static final String FRONT_ADDRESS = "http://dev.uptalent.pepega.cloud/api/v1/sponsors/";
 
@@ -18,5 +41,7 @@ public class EmailConstant {
     private void setAdminMail(){
         EmailConstant.ADMIN_MAIL = this.email;
     }
+
+
 
 }
--- src/main/java/com/uptalent/email/EmailSender.java
+++ src/main/java/com/uptalent/email/EmailSender.java
@@ -1,54 +1,36 @@
 package com.uptalent.email;
 
+import com.uptalent.email.model.EmailType;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.UUID;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@EnableAsync
 public class EmailSender{
     private final JavaMailSender sender;
-
-    public void sendMail(String email, String token, HttpServletRequest request, String fullname, LocalDateTime localDateTime) throws MessagingException {
-        MimeMessage mail = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mail);
-        String refererAddress = request.getHeader(HttpHeaders.REFERER);
-        // String servletPath = request.getServletPath();
-        String url = refererAddress + "restore?token=" + token;
-        helper.setFrom(EmailConstant.ADMIN_MAIL);
-        helper.setTo(email);
-        //helper.setTo("uptalentinfo@gmail.com");
-        helper.setSubject(EmailConstant.SUBJECT);
-        helper.setText("""
-                        <div style="display: grid;\040
-                        flex-direction: column;\040
-                        border-radius: 10px;\040
-                        padding: 20px;\040
-                        font-size: larger;\040
-                        border: 3px solid #48bde2;\040
-                        margin: 3px;">
-                        <h1 style="color: #48bde2;\040
-                        margin: 0;">
-                            Restore your account
+    public MimeMessageHelper chooseMessage(MimeMessageHelper helper, EmailType type, String fullname, LocalDateTime localDateTime, String token, String refererAddress) throws MessagingException {
+        String message_body;
+        String url;
+        if(type == EmailType.RESTORE){
+            helper.setSubject(EmailConstant.SUBJECT_RESTORE);
+            url = refererAddress + "restore?token=" + token;
+            message_body = """
+                    Restore your account
                         </h1>
                         <h3 style="margin: 10px 0;">
                             Dear %s,
@@ -71,17 +53,55 @@ public class EmailSender{
                             Restore your account
                         </button>
                         </a>
-                        <h4 style="margin-bottom: 0;">
-                            Best regards,
-                        </h4>
-                        <div style="display: flex; align-items: center; gap: 5px;">
-                        <img src="https://drive.google.com/uc?export=view&id=1Fx1E7h2r8ly23p5LCD5Jpc_slgUPSu2I" alt="logo" style="width:30px; object-fit: contain;"/>
-                        <h4 style="margin-left: 5px;">
-                            UPTALENT
-                        </h4>
-                        </div>
-                        </div>
-                """.formatted(fullname, localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE), url), true);
+                """;
+        }
+        else{
+            helper.setSubject(EmailConstant.SUBJECT_VERIFY);
+            url = refererAddress + "verify?token=" + token;
+            message_body = """
+                        Verify your account
+                        </h1>
+                        <h3 style="margin: 10px 0;">
+                            Dear %s,
+                        </h3>
+                        <p>
+                            Someone created an account with your email
+                        </p>
+                        <p>
+                            If it wasn't you, then you don't need to do anything.
+                        </p>
+                        <p>
+                            If it you, please activate it until <b>%s</b>
+                        </p>
+                        <p>\040
+                            Please click the button down bellow and follow the instructions.
+                        </p>
+                        <a href="%s" style="width: 200px; height: 50px; align-self: center;">
+                        <button style="background: linear-gradient(87.27deg, rgb(156, 218, 237) 3.18%%, rgb(0, 147, 193) 63.05%%);
+                        cursor: pointer;\040
+                        color:white;\040
+                        font-weight: bold;\040
+                        width: 200px;\040
+                        height: 50px;\040
+                        border: none;\040
+                        border-radius: 10px;">
+                            Verify your account
+                        </button>
+                        </a>
+                    """;
+        }
+        helper.setText(EmailConstant.MESSAGE_BEGIN + message_body.formatted(fullname, localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE), url) + EmailConstant.MESSAGE_END, true);
+        return helper;
+    }
+    @Async
+    public void sendMail(String email, String token, String refererAddress, String fullname, LocalDateTime localDateTime, EmailType type) throws MessagingException {
+        MimeMessage mail = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mail);
+        // String servletPath = request.getServletPath();
+        helper = chooseMessage(helper, type, fullname, localDateTime, token, refererAddress);
+        helper.setFrom(EmailConstant.ADMIN_MAIL);
+        //helper.setTo(email);
+        helper.setTo("uptalentinfo@gmail.com");
         try {
             sender.send(mail);
         }
