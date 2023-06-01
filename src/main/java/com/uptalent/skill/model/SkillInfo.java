30223992e8e3e95ef4a43755ed9b386116494440
Vadym <vadym.pratsybuda@nure.ua>
2023-06-01 23:43:33 +0300

    Add empty constructor to SkillInfo

--- src/main/java/com/uptalent/skill/model/SkillInfo.java
+++ src/main/java/com/uptalent/skill/model/SkillInfo.java
@@ -2,9 +2,11 @@ package com.uptalent.skill.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SkillInfo {
     private String name;
     private Long kudos;
