eda05adcf70f228115126a269610a6ceb09343a8
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-06-09 12:36:34 +0300

    Create mapper VacancyMapper

---
+++ src/main/java/com/uptalent/mapper/VacancyMapper.java
@@ -0,0 +1,34 @@
+package com.uptalent.mapper;
+
+import com.uptalent.proof.model.enums.ContentStatus;
+import com.uptalent.skill.model.SkillVacancyInfo;
+import com.uptalent.vacancy.Vacancy;
+import com.uptalent.vacancy.VacancyDetailInfo;
+import com.uptalent.vacancy.VacancyModify;
+import org.mapstruct.Mapper;
+
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface VacancyMapper {
+    default Vacancy toVacancy(VacancyModify vacancyModify) {
+        return Vacancy.builder()
+                .title(vacancyModify.getTitle())
+                .content(vacancyModify.getContent())
+                .status(ContentStatus.valueOf(vacancyModify.getStatus()))
+                .build();
+    }
+
+    default VacancyDetailInfo toVacancyDetailInfo(Vacancy vacancy) {
+        return VacancyDetailInfo.builder()
+                .id(vacancy.getId())
+                .title(vacancy.getTitle())
+                .content(vacancy.getContent())
+                .published(vacancy.getPublished())
+                .status(vacancy.getStatus())
+                .skills(vacancy.getSkills().stream()
+                        .map(skill -> new SkillVacancyInfo(skill.getName()))
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+}
