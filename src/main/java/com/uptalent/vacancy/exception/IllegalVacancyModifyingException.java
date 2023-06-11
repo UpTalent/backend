f7600ce01c82c8568883c4dadadd6bcadcd8b0f6
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-11 12:39:11 +0300

    Delete IllegalVacancyModifyingException.java

--- src/main/java/com/uptalent/vacancy/exception/IllegalVacancyModifyingException.java
+++
@@ -1,7 +0,0 @@
-package com.uptalent.vacancy.exception;
-
-public class IllegalVacancyModifyingException extends RuntimeException {
-    public IllegalVacancyModifyingException(String message) {
-        super(message);
-    }
-}
