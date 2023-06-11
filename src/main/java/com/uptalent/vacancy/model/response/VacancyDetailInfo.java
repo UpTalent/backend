1b714d588fd6ade9984d4b8da39120a0e4065c4f
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-11 16:52:28 +0300

    Update VacancyDetailInfo

--- src/main/java/com/uptalent/vacancy/model/response/VacancyDetailInfo.java
+++ src/main/java/com/uptalent/vacancy/model/response/VacancyDetailInfo.java
@@ -3,6 +3,7 @@ package com.uptalent.vacancy.model.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.uptalent.proof.model.enums.ContentStatus;
 import com.uptalent.skill.model.SkillVacancyInfo;
+import com.uptalent.util.model.response.Author;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,4 +22,5 @@ public class VacancyDetailInfo {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime published;
     private Set<SkillVacancyInfo> skills;
+    private Author author;
 }
