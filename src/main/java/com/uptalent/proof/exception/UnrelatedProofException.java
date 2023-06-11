38e7a29364a59bcfba0b7bdd75ae6400b7c361d3
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-11 12:38:59 +0300

    Delete UnrelatedProofException.java

--- src/main/java/com/uptalent/proof/exception/UnrelatedProofException.java
+++
@@ -1,7 +0,0 @@
-package com.uptalent.proof.exception;
-
-public class UnrelatedProofException extends RuntimeException {
-    public UnrelatedProofException(String message) {
-        super(message);
-    }
-}
