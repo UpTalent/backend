2e0fc676fbbea96bbec22b28913b2f8ff22aea39
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-11 16:52:06 +0300

    Update ProofTalentGeneralInfo

--- src/main/java/com/uptalent/proof/model/response/ProofTalentGeneralInfo.java
+++ src/main/java/com/uptalent/proof/model/response/ProofTalentGeneralInfo.java
@@ -1,6 +1,7 @@
 package com.uptalent.proof.model.response;
 
 import com.uptalent.skill.model.SkillProofInfo;
+import com.uptalent.util.model.response.Author;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,7 +15,7 @@ public class ProofTalentGeneralInfo extends ProofGeneralInfo {
 
     public ProofTalentGeneralInfo(Long id, Integer iconNumber, String title,
                                   String summary, long kudos, LocalDateTime published, boolean myProof,
-                                  Set<SkillProofInfo> skills, AuthorProof author) {
+                                  Set<SkillProofInfo> skills, Author author) {
         super(id, iconNumber, title, summary, kudos, published, skills, author);
         this.myProof = myProof;
     }
