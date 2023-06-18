11a72801a339d19b9d8f577d958ac4b68d2df012
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-06-18 21:15:05 +0300

    Add query for getting vacancies by sponsor-id

--- src/main/java/com/uptalent/vacancy/repository/VacancyRepository.java
+++ src/main/java/com/uptalent/vacancy/repository/VacancyRepository.java
@@ -1,9 +1,19 @@
 package com.uptalent.vacancy.repository;
 
+import com.uptalent.proof.model.enums.ContentStatus;
 import com.uptalent.vacancy.model.entity.Vacancy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
+    @Query("SELECT v " +
+            "FROM vacancy v " +
+            "WHERE v.status = :contentStatus AND v.sponsor.id = :sponsorId")
+    Page<Vacancy> findVacanciesBySponsorId(Long sponsorId,
+                                                    ContentStatus contentStatus,
+                                                    Pageable pageable);
 }
