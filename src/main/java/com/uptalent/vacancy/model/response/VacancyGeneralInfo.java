22385dc0d21662e5eb15afac31460ee8331a761c
CherkasovAndrii <andreyc406@gmail.com>
2023-06-22 12:33:30 +0300

    Create response model VacancyGeneralInfo

---
+++ src/main/java/com/uptalent/vacancy/model/response/VacancyGeneralInfo.java
@@ -0,0 +1,23 @@
+package com.uptalent.vacancy.model.response;
+
+import com.uptalent.skill.model.SkillVacancyInfo;
+import com.uptalent.util.model.response.Author;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class VacancyGeneralInfo {
+    private Long id;
+    private String title;
+    private LocalDateTime published;
+    private Set<SkillVacancyInfo> skills;
+    private Author author;
+
+
+}
