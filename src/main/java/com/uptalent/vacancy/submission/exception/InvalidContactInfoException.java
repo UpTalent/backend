290a882e837d2830f8ee6d3493d44dd67e682d31
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-07 18:15:32 +0300

    Add InvalidContactInfoException for input validation

---
+++ src/main/java/com/uptalent/vacancy/submission/exception/InvalidContactInfoException.java
@@ -0,0 +1,7 @@
+package com.uptalent.vacancy.submission.exception;
+
+public class InvalidContactInfoException extends RuntimeException{
+    public InvalidContactInfoException(String message){
+        super(message);
+    }
+}
