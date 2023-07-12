72eec5ffe90cb6fb3ba8f3a09308bd7df18ed9e8
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-12 14:32:55 +0300

    Replaced filtering with SubmissionRepository query to improve performance

--- src/main/java/com/uptalent/vacancy/service/VacancyService.java
+++ src/main/java/com/uptalent/vacancy/service/VacancyService.java
@@ -93,8 +93,8 @@ public class VacancyService {
                 .toUri();
     }
 
-    public VacancyDetailInfo getVacancy(Long id) {
-        Vacancy vacancy = getVacancyById(id);
+    public VacancyDetailInfo getVacancy(Long vacancyId) {
+        Vacancy vacancy = getVacancyById(vacancyId);
 
         if(!vacancy.getStatus().equals(PUBLISHED))
             accessVerifyService.tryGetAccess(vacancy.getSponsor().getId(), SPONSOR,
@@ -107,12 +107,7 @@ public class VacancyService {
             boolean canSubmit = hasMatchedSkills(talent, vacancy);
             talentVacancyDetailInfo.setCanSubmit(canSubmit);
 
-            Long talentId = talent.getId();
-            Optional<Submission> talentSubmission = vacancy.getSubmissions()
-                    .stream()
-                    .filter(submission -> submission.getTalent().getId().equals(talentId))
-                    .findFirst();
-
+            Optional<Submission> talentSubmission = submissionRepository.findSubmissionByTalentIdAndVacancyId(talent.getId(), vacancyId);
             talentSubmission.ifPresent(submission -> talentVacancyDetailInfo
                     .setMySubmission(vacancyMapper.toSubmissionResponse(submission))
             );
@@ -229,7 +224,7 @@ public class VacancyService {
     public PageWithMetadata<TalentSubmission> getTalentSubmissions(int page, int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
         Long talentId = accessVerifyService.getPrincipalId();
-        Page<Submission> submissionsPage = submissionRepository.findSubmissionByTalentId(pageRequest, talentId);
+        Page<Submission> submissionsPage = submissionRepository.findSubmissionsByTalentId(pageRequest, talentId);
 
         List<TalentSubmission> talentSubmissions = submissionsPage
                 .stream()
