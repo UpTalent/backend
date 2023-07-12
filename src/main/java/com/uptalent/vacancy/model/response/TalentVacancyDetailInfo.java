06b9d7b01a19dd5836a9b0c9a34fe291998c9652
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-12 14:18:15 +0300

    Add SubmissionResponse to TalentVacancyDetailInfo

--- src/main/java/com/uptalent/vacancy/model/response/TalentVacancyDetailInfo.java
+++ src/main/java/com/uptalent/vacancy/model/response/TalentVacancyDetailInfo.java
@@ -1,5 +1,6 @@
 package com.uptalent.vacancy.model.response;
 
+import com.uptalent.vacancy.submission.model.response.SubmissionResponse;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
@@ -8,5 +9,6 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 public class TalentVacancyDetailInfo extends VacancyDetailInfo {
+    private SubmissionResponse mySubmission;
     private boolean canSubmit;
 }
