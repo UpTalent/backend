e882a913398378b2c076ff9e4233d735a92c999a
Vadym <vadym.pratsybuda@nure.ua>
2023-07-13 14:37:38 +0300

    Create Answer controller, service, repository

---
+++ src/main/java/com/uptalent/answer/controller/AnswerController.java
@@ -0,0 +1,37 @@
+package com.uptalent.answer.controller;
+
+import com.uptalent.answer.model.request.TemplateMessageRequest;
+import com.uptalent.answer.service.AnswerService;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/answers")
+@Validated
+@Tag(name = "Answer", description = "Answer APIs documentation")
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
+public class AnswerController {
+    private final AnswerService answerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    void createTemplateMessage(@Valid @RequestBody TemplateMessageRequest templateMessageRequest){
+        answerService.createTemplate(templateMessageRequest);
+    }
+
+}
---
+++ src/main/java/com/uptalent/answer/service/AnswerService.java
@@ -0,0 +1,63 @@
+package com.uptalent.answer.service;
+
+import com.uptalent.answer.model.entity.Answer;
+import com.uptalent.answer.model.enums.MessageStatus;
+import com.uptalent.answer.model.request.TemplateMessageRequest;
+import com.uptalent.answer.repository.AnswerRepository;
+import com.uptalent.proof.model.enums.ContentStatus;
+import com.uptalent.sponsor.exception.SponsorNotFoundException;
+import com.uptalent.sponsor.model.entity.Sponsor;
+import com.uptalent.sponsor.repository.SponsorRepository;
+import com.uptalent.util.service.AccessVerifyService;
+import com.uptalent.vacancy.submission.exception.InvalidContactInfoException;
+import com.uptalent.vacancy.submission.model.enums.SubmissionStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.regex.Pattern;
+
+import static com.uptalent.util.RegexValidation.*;
+import static com.uptalent.util.RegexValidation.isValidLinkedInUrl;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+    private final AccessVerifyService accessVerifyService;
+    private final SponsorRepository sponsorRepository;
+    @PreAuthorize("hasAuthority('SPONSOR')")
+    @Transactional
+    public void createTemplate(TemplateMessageRequest templateMessageRequest) {
+        Long sponsorId = accessVerifyService.getPrincipalId();
+
+        Sponsor sponsor = sponsorRepository.findById(sponsorId)
+                .orElseThrow(() -> new SponsorNotFoundException("Sponsor was not found"));
+
+        isValidContactInfo(templateMessageRequest.getContactInfo());
+
+        var answer = Answer.builder()
+                .contactInfo(templateMessageRequest.getContactInfo())
+                .isTemplatedMessage(templateMessageRequest.getIsTemplatedMessage())
+                .status(MessageStatus.valueOf(templateMessageRequest.getStatus()))
+                .title(templateMessageRequest.getTitle())
+                .message(templateMessageRequest.getMessage())
+                .sponsor(sponsor)
+                .build();
+
+        answerRepository.save(answer);
+    }
+    private void isValidContactInfo(String contactInfo) {
+        if(isValidEmail(contactInfo)
+                || isValidPhone(contactInfo)
+                || isValidTelegramUrl(contactInfo)
+                || isValidLinkedInUrl(contactInfo)){
+            return;
+        }
+        throw new InvalidContactInfoException("Invalid contact info");
+    }
+}
