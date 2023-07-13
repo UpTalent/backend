dc1ab87b0f52dcc9650917b2f5cc82e008aa9227
Vadym <vadym.pratsybuda@nure.ua>
2023-07-13 14:37:54 +0300

    Add migration

---
+++ src/main/resources/db/migration/V16__add-answer.sql
@@ -0,0 +1,11 @@
+CREATE TABLE answer
+(
+    id SERIAL PRIMARY KEY,
+    contact_info VARCHAR(100) NOT NULL,
+    message VARCHAR(1000) NOT NULL,
+    title VARCHAR(255),
+    status VARCHAR(20) NOT NULL,
+    sponsor_id BIGINT,
+    is_templated_message BOOLEAN,
+    FOREIGN KEY (sponsor_id) REFERENCES sponsor (id)
+);
