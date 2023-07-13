4a8472878f418365a52a827428833802f68dc660
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-13 20:59:01 +0300

    Create exception and handle

---
+++ src/main/java/com/uptalent/answer/exception/AnswerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.uptalent.answer.exception;
+
+public class AnswerNotFoundException extends RuntimeException {
+    public AnswerNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/uptalent/vacancy/exception/handler/VacancyExceptionHandler.java
+++ src/main/java/com/uptalent/vacancy/exception/handler/VacancyExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.uptalent.vacancy.exception.handler;
 
+import com.uptalent.answer.exception.AnswerNotFoundException;
 import com.uptalent.payload.HttpResponse;
 import com.uptalent.util.exception.handler.ExceptionHandlerController;
 import com.uptalent.vacancy.exception.NoSuchMatchedSkillsException;
 import com.uptalent.vacancy.exception.VacancyNotFoundException;
 import com.uptalent.vacancy.submission.exception.DuplicateSubmissionException;
+import com.uptalent.vacancy.submission.exception.IllegalSubmissionException;
 import com.uptalent.vacancy.submission.exception.InvalidContactInfoException;
+import com.uptalent.vacancy.submission.exception.SubmissionNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,8 +17,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class VacancyExceptionHandler extends ExceptionHandlerController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(VacancyNotFoundException.class)
-    public HttpResponse handlerVacancyNotFoundException(VacancyNotFoundException e) {
+    @ExceptionHandler({VacancyNotFoundException.class,
+            SubmissionNotFoundException.class,
+            AnswerNotFoundException.class})
+    public HttpResponse handlerVacancyNotFoundException(RuntimeException e) {
         return new HttpResponse(e.getMessage());
     }
 
@@ -26,8 +31,9 @@ public class VacancyExceptionHandler extends ExceptionHandlerController {
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(DuplicateSubmissionException.class)
-    public HttpResponse handlerDuplicateSubmissionException(DuplicateSubmissionException e) {
+    @ExceptionHandler({DuplicateSubmissionException.class,
+            IllegalSubmissionException.class})
+    public HttpResponse handlerDuplicateSubmissionException(RuntimeException e) {
         return new HttpResponse(e.getMessage());
     }
 
