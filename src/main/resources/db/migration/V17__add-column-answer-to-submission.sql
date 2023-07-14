34022e0f5058f57126965b63f3324dde93b3b5f3
Dmytro Teliukov <77294463+DmytroTeliukov@users.noreply.github.com>
2023-07-14 18:29:37 +0300

    Fix migration V17 (#254)
    * Create response model FeedbackResponse
    
    * Add field author response model SubmissionResponse
    
    * Add field feedbackResponse response model TalentSubmission
    
    * Create method toFeedbackResponse
    
    * Change data retrieving in method getTalentSubmissions
    
    * Fix mapping submission response
    
    * Fix migration V17

--- src/main/resources/db/migration/V17__add-column-answer-to-submission.sql
+++ src/main/resources/db/migration/V17__add-column-answer-to-submission.sql
@@ -1,4 +1,4 @@
 ALTER TABLE submission
-    ADD COLUMN answer_id bigint not null;
+    ADD COLUMN answer_id bigint null;
 ALTER TABLE submission ADD CONSTRAINT answer_submission_id_fkey
     foreign key (answer_id) REFERENCES answer(id) on delete cascade on update cascade;
