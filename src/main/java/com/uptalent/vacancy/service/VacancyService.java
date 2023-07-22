f10addf385a235e9d33f1920ffbbb287d38aa2e6
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-22 21:41:02 +0300

    Fix response of posting submission

--- src/main/java/com/uptalent/vacancy/service/VacancyService.java
+++ src/main/java/com/uptalent/vacancy/service/VacancyService.java
@@ -35,6 +35,7 @@ import com.uptalent.vacancy.submission.exception.SubmissionNotFoundException;
 import com.uptalent.vacancy.submission.model.entity.Submission;
 import com.uptalent.vacancy.submission.model.enums.SubmissionStatus;
 import com.uptalent.vacancy.submission.model.request.SubmissionRequest;
+import com.uptalent.vacancy.submission.model.response.FullSubmissionResponse;
 import com.uptalent.vacancy.submission.model.response.SubmissionResponse;
 import com.uptalent.vacancy.submission.model.response.TalentSubmission;
 import com.uptalent.vacancy.submission.repository.SubmissionRepository;
@@ -210,7 +211,7 @@ public class VacancyService {
     }
 
     @Transactional
-    public SubmissionResponse createSubmission(Long vacancyId, SubmissionRequest submissionRequest) {
+    public FullSubmissionResponse createSubmission(Long vacancyId, SubmissionRequest submissionRequest) {
         Vacancy vacancy = getVacancyById(vacancyId);
         Talent talent = getTalentById(accessVerifyService.getPrincipalId());
 
@@ -227,7 +228,7 @@ public class VacancyService {
         submission.setSent(LocalDateTime.now());
         submission.setStatus(SENT);
 
-        return vacancyMapper.toSubmissionResponse(submissionRepository.save(submission));
+        return vacancyMapper.toFullSubmissionResponse(submissionRepository.save(submission));
     }
 
     public PageWithMetadata<TalentSubmission> getTalentSubmissions(int page, int size) {
