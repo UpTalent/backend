95b8370a72d6ff7b901d901c684f9c4b4d57fe8c
Dmytro Teliukov <77294463+DmytroTeliukov@users.noreply.github.com>
2023-07-22 12:41:23 +0300

    Edit SubmissionResponse and update mapper (#255)

--- src/main/java/com/uptalent/vacancy/submission/model/response/SubmissionResponse.java
+++ src/main/java/com/uptalent/vacancy/submission/model/response/SubmissionResponse.java
@@ -1,6 +1,5 @@
 package com.uptalent.vacancy.submission.model.response;
 
-import com.uptalent.util.model.response.Author;
 import com.uptalent.vacancy.submission.model.enums.SubmissionStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,9 +9,6 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class SubmissionResponse {
-    private String contactInfo;
-    private String message;
     private LocalDateTime sent;
     private SubmissionStatus status;
-    private Author author;
 }
