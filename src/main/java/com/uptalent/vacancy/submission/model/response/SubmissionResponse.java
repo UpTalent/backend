e8bb75c8d8236d52edfb6ebdb3991367f35fbdea
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-23 14:21:17 +0300

    Add 'id' field to SubmissionResponse model

--- src/main/java/com/uptalent/vacancy/submission/model/response/SubmissionResponse.java
+++ src/main/java/com/uptalent/vacancy/submission/model/response/SubmissionResponse.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SubmissionResponse {
+    private long id;
     private LocalDateTime sent;
     private SubmissionStatus status;
 }
